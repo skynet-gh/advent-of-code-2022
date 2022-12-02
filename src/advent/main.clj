27130e449f1eba824615f885a0113e2bac4d03aa
skynet <skynet-gh@users.noreply.github.com>
2022-12-02 00:14:27 -0500

    Clean up day01 clj

--- src/advent/main.clj
+++ src/advent/main.clj
@@ -6,25 +6,26 @@
 
 
 (defn day1 []
-  (let [elves (-> "day01.txt" io/resource slurp (string/split #"\n\n"))]
-    (->> elves
-         (map
-           (fn [foods]
-             (->> (string/split foods #"\n")
-                  (map parse-long)
-                  (reduce + 0)))))))
-(defn day1-part1 []
-  (reduce max (day1)))
-
-(defn day1-part2 []
-  (->> (day1)
+  (->> (-> "day01.txt" io/resource slurp (string/split #"\n\n"))
+       (mapv
+         (fn [foods]
+           (->> (string/split foods #"\n")
+                (map parse-long)
+                (reduce + 0))))
        sort
-       reverse
+       reverse))
+
+(defn day1-part1 [sorted-elves]
+  (last sorted-elves))
+
+(defn day1-part2 [sorted-elves]
+  (->> sorted-elves
        (take 3)
        (reduce + 0)))
 
 (defn -main [& _]
   (println "Advent of code 2022")
-  (println "Day 01")
-  (println " Part 1:" (day1-part1))
-  (println " Part 2:" (day1-part2)))
+  (let [elves (day1)]
+    (println "Day 01")
+    (println " Part 1:" (day1-part1 elves))
+    (println " Part 2:" (day1-part2 elves))))
